06b38ca030d3b5994f4eadaccab316714018955d
ChiaoChiehChen <qa6248@gmail.com>
2022-02-11 09:37:03 +0800

    table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,77 +1,81 @@
 <template>
   <div id="home">
     <div>
-    <swiper class="swiper chiao_swiper" :options="swiperOption">
-      <swiper-slide>
-        <v-parallax
-          class="swiper_img"
-          height="1400"
-          src="../assets/swiper2.jpg"
-        ></v-parallax>
-        <v-img></v-img>
-      </swiper-slide>
-      <swiper-slide>
-        <v-parallax
-        class="swiper_img"
-        height="1400"
-        src="../assets/swiper1.jpg"></v-parallax>
-      </swiper-slide>
-      <swiper-slide>
-        <v-parallax
-        class="swiper_img"
-        height="1400"
-        src="../assets/swiper4.jpg"></v-parallax>
-      </swiper-slide>
-      <swiper-slide>
-        <v-parallax
-        class="swiper_img"
-        height="1400"
-        src="../assets/swiper3.jpg"></v-parallax>
-      </swiper-slide>
-      <div class="swiper-pagination" slot="pagination"></div>
-      <div class="swiper-button-prev" slot="button-prev"></div>
-      <div class="swiper-button-next" slot="button-next"></div>
-    </swiper>
+      <swiper class="swiper chiao_swiper" :options="swiperOption">
+        <swiper-slide>
+          <v-parallax class="swiper_img" height="1400" src="../assets/swiper2.jpg"></v-parallax>
+          <v-img></v-img>
+        </swiper-slide>
+        <swiper-slide>
+          <v-parallax class="swiper_img" height="1400" src="../assets/swiper1.jpg"></v-parallax>
+        </swiper-slide>
+        <swiper-slide>
+          <v-parallax class="swiper_img" height="1400" src="../assets/swiper4.jpg"></v-parallax>
+        </swiper-slide>
+        <swiper-slide>
+          <v-parallax class="swiper_img" height="1400" src="../assets/swiper3.jpg"></v-parallax>
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+      </swiper>
     </div>
 
     <v-container id="second" class="home_coffee ma-0 pa-0" fluid>
       <v-row justify="center" align-content="center">
         <v-col class="home_col text-center" cols="6" md="3">
-            <router-link to="/">
-                <img class="beans" src="../assets/cafebean.png" alt="">
-                <p class="text-center" color="secondary">coffee powder</p>
-            </router-link>
+          <router-link to="/">
+            <img class="beans" src="../assets/cafebean.png" alt />
+            <p class="text-center" color="secondary">coffee powder</p>
+          </router-link>
         </v-col>
         <v-col class="home_col text-center" cols="6" md="3" align-self="center">
-            <router-link to="/">
-                <img class="powder" src="../assets/coffeepowder.png" alt="">
-                <p class="text-center" color="secondary">coffee powder</p>
-            </router-link>
+          <router-link to="/">
+            <img class="powder" src="../assets/coffeepowder.png" alt />
+            <p class="text-center" color="secondary">coffee powder</p>
+          </router-link>
         </v-col>
         <v-col class="home_col text-center" cols="6" md="3" align-self="end">
-            <router-link to="/">
-                <img class="cupsule" src="../assets/cupsulecoffee.png" alt="">
-                <p class="text-center" color="secondary">coffee powder</p>
-            </router-link>
+          <router-link to="/">
+            <img class="cupsule" src="../assets/cupsulecoffee.png" alt />
+            <p class="text-center" color="secondary">coffee powder</p>
+          </router-link>
         </v-col>
-        <v-col class=" home_coltext-center" cols="6" md="3" align-self="center">
-            <router-link to="/">
-                <img class="powder" src="../assets/coffeepowder.png" alt="">
-                <p class="text-center" color="secondary">coffee powder</p>
-            </router-link>
+        <v-col class="home_coltext-center" cols="6" md="3" align-self="center">
+          <router-link to="/">
+            <img class="powder" src="../assets/coffeepowder.png" alt />
+            <p class="text-center" color="secondary">coffee powder</p>
+          </router-link>
         </v-col>
       </v-row>
     </v-container>
-    <v-container class="third">
-      <h1>NEWS</h1>
-      <v-row>
-        <v-col></v-col>
+    <v-container id="third">
+      <h1 class="text-center">NEWS</h1>
+      <v-row class="pt-16">
+        <v-card width="500">
+          <v-simple-table>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">Name</th>
+                  <th class="text-left">Calories</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in desserts" :key="item.name">
+                  <td>{{ item.name }}</td>
+                  <td>{{ item.calories }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-card>
       </v-row>
     </v-container>
     <v-container id="forth">
       <v-row>
         <v-col class="text-center" cols="12" md="6" order-md="2">
-          <img src="https://picsum.photos/450/600/?random=10">
+          <img src="https://picsum.photos/450/600/?random=10" />
         </v-col>
         <v-col class="home_intro" cols="12" md="6" order-md="1">
           <div>
@@ -81,8 +85,7 @@
         </v-col>
       </v-row>
     </v-container>
-    <v-container>
-    </v-container>
+    <v-container></v-container>
   </div>
 </template>
 
@@ -116,7 +119,41 @@ export default {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev'
         }
-      }
+      },
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237
+        },
+        {
+          name: 'Eclair',
+          calories: 262
+        },
+        {
+          name: 'Cupcake',
+          calories: 305
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375
+        },
+        {
+          name: 'Lollipop',
+          calories: 392
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408
+        }
+      ]
     }
   }
 }
