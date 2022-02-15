b6a76eaabbb50a00b33fae3bbe80ec25996c42eb
ChiaoChiehChen <qa6248@gmail.com>
2022-02-15 16:33:28 +0800

    adminProducts

--- src/views/AdminProducts.vue
+++ src/views/AdminProducts.vue
@@ -2,11 +2,17 @@
   <div id="adminProducts">
     <v-container>
       <v-row justify="center">
-        <v-dialog v-model="dialog" @save="submitSave" max-width="700px">
+        <v-dialog v-model="dialog" max-width="700px">
           <template v-slot:activator="{ on, adds }">
             <v-btn v-bind="adds" v-on="on">新增</v-btn>
           </template>
           <v-card>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn icon x-large @click="dialog = false">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </v-card-actions>
             <v-card-title v-if="form._id.length > 0">
               <span>編輯商品</span>
             </v-card-title>
@@ -15,87 +21,96 @@
             </v-card-title>
             <v-divider></v-divider>
             <v-card-text>
-              <v-row>
-                <v-col cols="12">
-                  <v-text-field
-                    v-model="form.name"
-                    label="商品名稱"
-                    type="text"
-                    :rules="nameRules"
-                    hint="必填欄位"
-                    required
-                    clear-icon="mdi-close-circle"
-                    clearable
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field
-                    v-model="form.price"
-                    label="價錢"
-                    type="number"
-                    min="0"
-                    :rules="priceRules"
-                    hint="必填欄位"
-                    required
-                    append-icon="mdi-currency-twd"
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" md="6">
-                  <v-autocomplete
-                    v-model="form.category.big"
-                    label="商品種類"
-                    :items="Object.keys(categories)"
-                    outlined
-                    dense
-                  ></v-autocomplete>
-                </v-col>
-                <v-col cols="12" md="6">
-                  <v-autocomplete
-                    v-model="form.category.small"
-                    label="商品項目"
-                    :items="categories[form.category.big]"
-                    outlined
-                    dense
-                  ></v-autocomplete>
-                </v-col>
-                <v-col cols="12">
-                  <v-textarea
-                    name="input-7-1"
-                    v-model="form.description"
-                    label="商品說明"
-                    hint="需填寫描述"
-                    clear-icon="mdi-close-circle"
-                    clearable
-                    outlined
-                    auto-grow
-                  ></v-textarea>
-                </v-col>
-                <v-col cols="12">
-                  <img-inputer
-                    class="img_file"
-                    accept="image/*"
-                    v-model="form.image"
-                    theme="light"
-                    size="large"
-                    :max-size="1024"
-                    exceed-size-text="文件大小不能超過"
-                    placeholder="點擊或拖拽選擇圖片"
-                    bottom-text="點擊或拖拽圖片以修改"
-                    hover-text="點擊或拖拽圖片以修改"
-                  />
-                </v-col>
-                <v-col cols="12">
-                  <v-radio-group v-model="form.sell" row>
-                    <v-radio label="上架" :value="true"></v-radio>
-                    <v-radio label="下架" :value="false"></v-radio>
-                  </v-radio-group>
-                </v-col>
-              </v-row>
+              <v-form ref="observer">
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="form.name"
+                      label="商品名稱"
+                      type="text"
+                      :rules="nameRules"
+                      hint="必填欄位"
+                      required
+                      clear-icon="mdi-close-circle"
+                      clearable
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="form.price"
+                      label="價錢"
+                      type="number"
+                      min="0"
+                      :rules="priceRules"
+                      hint="必填欄位"
+                      required
+                      append-icon="mdi-currency-twd"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="6">
+                    <v-autocomplete
+                      v-model="form.category.big"
+                      label="商品種類"
+                      :items="Object.keys(categories)"
+                      :rules="categoryRules"
+                      hint="選項必填"
+                      outlined
+                      dense
+                    ></v-autocomplete>
+                  </v-col>
+                  <v-col cols="12" md="6">
+                    <v-autocomplete
+                      v-model="form.category.small"
+                      label="商品項目"
+                      :items="categories[form.category.big]"
+                      :rules="categoryRules"
+                      hint="選項必填"
+                      required
+                      outlined
+                      dense
+                    ></v-autocomplete>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-textarea
+                      name="input-7-1"
+                      v-model="form.description"
+                      label="商品說明"
+                      hint="需填寫描述"
+                      :rules="descriptionRules"
+                      required
+                      clear-icon="mdi-close-circle"
+                      clearable
+                      outlined
+                      auto-grow
+                    ></v-textarea>
+                  </v-col>
+                  <v-col cols="12">
+                    <img-inputer
+                      class="img_file"
+                      accept="image/*"
+                      v-model="form.image"
+                      theme="light"
+                      size="large"
+                      :max-size="1024"
+                      exceed-size-text="文件大小不能超過"
+                      placeholder="點擊或拖拽選擇圖片"
+                      bottom-text="點擊或拖拽圖片以修改"
+                      hover-text="點擊或拖拽圖片以修改"
+                    />
+                  </v-col>
+                  <v-col cols="12">
+                    <v-radio-group v-model="form.sell" row>
+                      <v-radio label="上架" :value="true"></v-radio>
+                      <v-radio label="下架" :value="false"></v-radio>
+                    </v-radio-group>
+                  </v-col>
+                </v-row>
+              </v-form>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn @click="dialog = true">送出</v-btn>
-              <v-btn @click="dialog = false">取消</v-btn>
+              <v-btn @click="resetForm" :disabled="btnSubmitting">重置</v-btn>
+              <v-btn @click="submitModal" :disabled="btnSubmitting">送出</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
@@ -110,7 +125,7 @@ export default {
     return {
       form: {
         name: '',
-        price: 0,
+        price: null,
         description: '',
         image: null,
         sell: false,
@@ -124,14 +139,74 @@ export default {
         設備: ['咖啡機', '濾紙', '咖啡壺', '濾杯']
       },
       nameRules: [
-        v => !!v || '必填',
-        v => (v && v.length > 1) || '商品名稱須1個字以上'
+        v => !!v || '商品名稱必填',
+        v => (v && v.length > 0) || '商品名稱必填欄位'
       ],
       priceRules: [
-        v => !!v || '必填',
-        v => (v && v.length >= 0) || '價格必須0元及以上'
+        v => !!v || '價格必填',
+        v => (v && v >= 0) || '價格不得低於0元'
       ],
+      categoryRules: [
+        v => !!v || '種類必選',
+        v => (v && v.length >= 0) || '種類欄位不得為空'
+      ],
+      descriptionRules: [
+        v => !!v || '商品描述必填',
+        v => (v && v.length >= 0) || '商品描述欄位不得為空'
+      ],
+      products: [],
+      btnSubmitting: false,
       dialog: false
+    }
+  },
+  methods: {
+    // 送出
+    async submitModal (event) {
+      event.preventDefault()
+      // 檢查必填欄位未填寫
+      if (!this.nameRules || !this.priceRules || !this.categoryRules || !this.descriptionRules) {
+        return
+      }
+      this.btnSubmitting = true
+      const fd = new FormData()
+      for (const key in this.form) {
+        if (key !== '_id') {
+          fd.append(key, this.form[key])
+        }
+      }
+      try {
+        const { data } = await this.api.post('/products', fd, {
+          headers: {
+            authorization: 'Bearer ' + this.user.token
+          }
+        })
+        this.products.push(data.result)
+        this.dialog = false
+      } catch (error) {
+        this.$swal({
+          icon: 'error',
+          title: '錯誤',
+          text: error.response.data.message
+        })
+      }
+      this.btnSubmitting = false
+    },
+    // 取消
+    resetForm (event) {
+      if (this.btnSubmitting) {
+        event.preventDefault()
+        return
+      }
+      this.form = {
+        name: '',
+        price: 0,
+        description: '',
+        image: null,
+        sell: false,
+        category: { big: '', small: '' },
+        _id: ''
+      }
+      this.$refs.observer.reset()
     }
   }
 }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,70 +1,72 @@
 <template>
-  <v-container id="login">
-    <h1 class="text-center">Welcome back</h1>
-    <div class="login_con">
-      <v-row class="mt-md-5 mt-lg-16">
-        <v-col cols="12" lg="2"></v-col>
-        <v-col class="d-flex justify-center align-center" cols="12" lg="4" >
-          <v-hover>
-            <template v-slot:default="{ hover }">
-              <v-card class="rounded-lg" width="400" height="400" :elevation="hover ? 10 : 2">
-                <v-card-title>
-                  <h1 class="mx-auto mt-5">Login</h1>
-                </v-card-title>
-                <v-divider class="ma-4"></v-divider>
-                <v-card-text>
-                  <v-form class="mt-5" v-model="valid" @submit.prevent="login" ref="form">
-                    <v-text-field
-                      v-model="form.account"
-                      label="My account"
-                      :rules="inputRules"
-                      :state="state.account"
-                      hint="帳號長度為 4 到 20 個字"
-                      prepend-icon="mdi-account-circle"
-                      required
-                    ></v-text-field>
+  <v-main>
+    <v-container id="login">
+      <h1 class="text-center">Welcome back</h1>
+      <div class="login_con">
+        <v-row class="mt-md-5 mt-lg-16">
+          <v-col cols="12" lg="2"></v-col>
+          <v-col class="d-flex justify-center align-center" cols="12" lg="4">
+            <v-hover>
+              <template v-slot:default="{ hover }">
+                <v-card class="rounded-lg" width="400" height="400" :elevation="hover ? 10 : 2">
+                  <v-card-title>
+                    <h1 class="mx-auto mt-5">Login</h1>
+                  </v-card-title>
+                  <v-divider class="ma-4"></v-divider>
+                  <v-card-text>
+                    <v-form class="mt-5" v-model="valid" @submit.prevent="login" ref="form">
+                      <v-text-field
+                        v-model="form.account"
+                        label="My account"
+                        :rules="inputRules"
+                        :state="state.account"
+                        hint="帳號長度為 4 到 20 個字"
+                        prepend-icon="mdi-account-circle"
+                        required
+                      ></v-text-field>
 
-                    <v-text-field
-                      v-model="form.password"
-                      label="My Password"
-                      :rules="inputRules"
-                      :state="state.password"
-                      hint="密碼長度為 4 到 20 個字"
-                      prepend-icon="mdi-lock"
-                      :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                      :type="showPassword ? 'text' : 'password'"
-                      @click:append="showPassword = !showPassword"
-                      required
-                    ></v-text-field>
-                    <v-action class="d-flex justify-center align-center">
-                      <v-btn class="login_btn mt-10" type="submit" plain>
-                        <span></span>
-                        <span></span>
-                        Login
-                      </v-btn>
-                    </v-action>
-                  </v-form>
-                </v-card-text>
-              </v-card>
-            </template>
-          </v-hover>
-        </v-col>
+                      <v-text-field
+                        v-model="form.password"
+                        label="My Password"
+                        :rules="inputRules"
+                        :state="state.password"
+                        hint="密碼長度為 4 到 20 個字"
+                        prepend-icon="mdi-lock"
+                        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                        :type="showPassword ? 'text' : 'password'"
+                        @click:append="showPassword = !showPassword"
+                        required
+                      ></v-text-field>
+                      <v-action class="d-flex justify-center align-center">
+                        <v-btn class="login_btn mt-10" type="submit" plain>
+                          <span></span>
+                          <span></span>
+                          Login
+                        </v-btn>
+                      </v-action>
+                    </v-form>
+                  </v-card-text>
+                </v-card>
+              </template>
+            </v-hover>
+          </v-col>
 
-        <v-col class="d-flex justify-center align-center" cols="12" lg="4">
-          <v-card class="reg_box" width="400" flat>
-            <v-card-title>
-              <h1>New to DOFECA ?</h1>
-            </v-card-title>
-            <v-card-text>
-              <p class="text-h6">Create an account to enjoy the benefits!</p>
-              <v-btn class="reg_btn mt-12" color="primary" plain text to="/register">Register</v-btn>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col cols="12" lg="2"></v-col>
-      </v-row>
-    </div>
-  </v-container>
+          <v-col class="d-flex justify-center align-center" cols="12" lg="4">
+            <v-card class="reg_box" width="400" flat>
+              <v-card-title>
+                <h1>New to DOFECA ?</h1>
+              </v-card-title>
+              <v-card-text>
+                <p class="text-h6">Create an account to enjoy the benefits!</p>
+                <v-btn class="reg_btn mt-12" color="primary" plain text to="/register">Register</v-btn>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12" lg="2"></v-col>
+        </v-row>
+      </div>
+    </v-container>
+  </v-main>
 </template>
 
 <script>
